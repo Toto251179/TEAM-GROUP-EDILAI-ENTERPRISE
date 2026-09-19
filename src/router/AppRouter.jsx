0d48f296f05abe86d8de7b58ec29d00cd80c47b6
Toto251179 <durante.sal@libero.it>
2026-09-19 09:42:00 +0200

    restore: abilita clienti prezziario e cantieri

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+
+import Clienti from "../pages/Clienti.jsx";
 import Preventivi from "../pages/Preventivi.jsx";
+import ElencoPrezzi from "../pages/ElencoPrezzi.jsx";
+import Cantieri from "../pages/Cantieri.jsx";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/preventivi" replace />} />
+      <Route path="/clienti" element={<Clienti />} />
       <Route path="/preventivi" element={<Preventivi />} />
+      <Route path="/elenco-prezzi" element={<ElencoPrezzi />} />
+      <Route path="/cantieri" element={<Cantieri />} />
       <Route path="*" element={<Navigate to="/preventivi" replace />} />
     </Routes>
   );
