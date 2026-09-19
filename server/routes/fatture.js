b74b7c56eef12058331a549940073e8003476775
Toto251179 <durante.sal@libero.it>
2026-09-19 09:23:00 +0200

    cleanup: rimuove API di modulo non utilizzato

--- server/routes/fatture.js
+++
@@ -1,9 +0,0 @@
-﻿import { createCrudRepository } from "../utils/crud.js";
-import { createCrudRouter } from "./createCrudRouter.js";
-
-const repository = createCrudRepository({
-  table: "fatture",
-  allowedFields: ["cantiereId", "clienteCode", "numero", "tipo", "data", "cantiere", "soggetto", "importo", "scadenza", "stato"],
-});
-
-export default createCrudRouter(repository);
